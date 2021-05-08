8a4ff0c093a6c3d297bad37812e156983a198158
Nobime <romeo.nobime@mailo.com>
2021-05-08 13:00:16 +0200

    Mise en place du server et modification src images

--- orinoco/frontend/src/index.js
+++ orinoco/frontend/src/index.js
@@ -1,2 +1,15 @@
-import msg from "./test.js";
-console.log(msg);
+fetch("http://localhost:3000/api/teddies")
+    .then
+    (
+        function(response)
+        {
+            console.log("pas d'erreur");
+        }
+    )
+    .catch
+    (
+        function(response)
+        {
+            console.log("erreur parce que voila");
+        }
+    )
--- orinoco/frontend/src/test.js
+++ orinoco/frontend/src/test.js
@@ -1,11 +1,11 @@
-fetch("http://localhost:3000/api/teddies")
+fetch("http://localhost:3000/api/teddies", {mode: 'no-cors'})
     .then
     (
         function(response)
         {
             if (response.ok)
             {
-                console.log(response);
+                console.log("pas d'erreur");
             }
         }
     )
