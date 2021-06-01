bae200d78bcd79c4032b3f617080865d78aaef09
Nobime <romeo.nobime@mailo.com>
2021-06-01 09:59:18 +0200

    Projet terminé

--- orinoco/frontend/src/commande.js
+++ orinoco/frontend/src/commande.js
@@ -101,7 +101,7 @@ if (window.location.pathname.indexOf("commande.html") != -1)
             /* On valide les données envoyées avec regex */
             const firstNameValidation = /[a-zA-Züäëïöûâêîôàèéç]/.test(firstName);
             const lastNameValidation = /[a-zA-Züäëïöûâêîôàèéç]/.test(lastName);
-            const addressValidation = /[a-zA-Züäëïöûâêîôàèéç]/.test(address);
+            const addressValidation = /[0-9a-zA-Züäëïöûâêîôàèéç]/.test(address);
             const cityValidation = /[a-zA-Züäëïöûâêîôàèéç]/.test(city);
             const emailValidation = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test(email);
 
